900a362687ce4a45257066404d817bc2abf7c116
baobao <wqm9425@gmail.com>
2017-03-19 22:16:36 -0400

    add selfpage

---
+++ app/src/main/java/group2/cs542/wpi/privateaudio/list_selfpage.java
@@ -0,0 +1,12 @@
+package group2.cs542.wpi.privateaudio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by baobao on 2017/3/19.
+ */
+
+public class list_selfpage extends Activity {
+
+}
