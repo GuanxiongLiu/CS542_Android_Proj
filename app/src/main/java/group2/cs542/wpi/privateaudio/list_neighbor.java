16633026db1b0753817ea2320035b6d1d016a22c
Guanxiong Liu <sylorbeijing@gmail.com>
2017-03-20 00:21:54 -0400

    achieve map center changes with location

--- app/src/main/java/group2/cs542/wpi/privateaudio/list_neighbor.java
+++ app/src/main/java/group2/cs542/wpi/privateaudio/list_neighbor.java
@@ -1,40 +1,59 @@
 package group2.cs542.wpi.privateaudio;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by sylor on 3/17/17.
  */
 
-public class list_neighbor extends FragmentActivity implements OnMapReadyCallback {
+public class list_neighbor extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
     private MapFragment mapF;
+    private Location lastLoc;
+    private GoogleMap mapHandle;
+    private LocationRequest mLocationRequest;
+    private Marker currentMaker;
+
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         LatLng HomeLoc = new LatLng(42.2746373, -71.8085277);
-        googleMap.addMarker(new MarkerOptions()
-                .position(HomeLoc)
-                .title("Maker"));
+        currentMaker = googleMap.addMarker(new MarkerOptions()
+                                            .position(HomeLoc)
+                                            .title("Initial"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HomeLoc, (float) 14.0));
+        mapHandle = googleMap;
     }
 
     @Override
@@ -43,7 +62,11 @@ public class list_neighbor extends FragmentActivity implements OnMapReadyCallbac
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        client = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .addApi(AppIndex.API).build();
 
         setContentView(R.layout.activity_neighbor);
 
@@ -87,5 +110,52 @@ public class list_neighbor extends FragmentActivity implements OnMapReadyCallbac
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+
+        startLocationUpdates();
+    }
+
+    protected void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(client, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lastLoc = location;
+        LatLng center = new LatLng(lastLoc.getLatitude(), lastLoc.getLongitude());
+        mapHandle.moveCamera(CameraUpdateFactory.newLatLngZoom(center, (float) 14.0));
+
+        currentMaker.remove();
+        currentMaker = mapHandle.addMarker(new MarkerOptions()
+                .position(center)
+                .title("Current"));
     }
 }
